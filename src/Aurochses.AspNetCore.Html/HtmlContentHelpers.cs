0ee6ed0e0e20e27d5c7921a5aad1bf0d11392688
Vladimir Rodchenko <vladimir@aurochses.com>
2017-11-01 22:54:18 +0300

    rename

--- src/Aurochses.AspNetCore.Html/HtmlContentHelpers.cs
+++ src/Aurochses.AspNetCore.Html/HtmlContentHelpers.cs
@@ -2,7 +2,7 @@
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Html;
 
-namespace Aurochses.Html
+namespace Aurochses.AspNetCore.Html
 {
     /// <summary>
     /// HtmlContentHelpers.
